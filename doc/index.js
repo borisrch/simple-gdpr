2d60e846b35f34e6b298f9df4b1c9ffcec955588
borisc1996j <borisc1996j@gmail.com>
2018-10-23 10:12:50 -0400

    update site w new sections, add table of contents, add emoji ref

--- doc/index.js
+++ doc/index.js
@@ -67,3 +67,85 @@ const demo4 = new SimpleGDPR({
     button: 'demo4-button',
   }
 });
+
+const usage1 = new SimpleGDPR({
+  float: 'usage1',
+  id: {
+    button: 'usage1',
+    box: 'usage1',
+  }
+});
+
+const usage2 = new SimpleGDPR({
+  float: 'usage2',
+  callback: () => {
+    alert('Hi!');
+    usage2.close();
+  },
+  id: {
+    button: 'usage2',
+    box: 'usage2',
+  }
+});
+
+const usage3 = new SimpleGDPR({
+  float: 'usage3',
+  title: 'This website uses cookies.',
+  link: './privacypolicy.html',
+  id: {
+    button: 'usage3',
+    box: 'usage3',
+  }
+});
+
+const usage4 = new SimpleGDPR({
+  float: 'usage4',
+  theme: 'modern',
+  icons: false,
+  animation: 'slide',
+  id: {
+    button: 'usage4',
+    box: 'usage4',
+  }
+});
+
+const usage5 = new SimpleGDPR({
+  title: 'Cookies and Cream',
+  message: 'Do you want some?',
+  link: null,
+  icons: true,
+  theme: 'material',
+  animation: 'slide',
+  float: 'usage-5',
+  callback: () => {
+    usage5.close();
+    setTimeout(() => {
+      usage5.open();
+    }, 1000);
+  },
+  openOnInit: true,
+  id: {
+    box: 'my-box-id',
+    button: 'my-button-id',
+  },
+})
+
+const group = document.createElement('div');
+const message = document.createElement('span');
+const policy = document.createElement('span');
+
+message.innerText = 'My custom message! ';       
+policy.innerText = 'Privacy Policy';
+policy.classList.add('sgdpr-policy');
+policy.addEventListener('click', () => {
+  alert('Here are my terms!');
+})
+
+group.appendChild(message);
+group.appendChild(policy);
+
+const message1 = new SimpleGDPR({
+  float:'message1',
+  message: group,
+});
+
